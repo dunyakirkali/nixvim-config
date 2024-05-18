cfd9008a029d2b7d529fc24faac2bad5bdf7870f
Dunya Kirkali <dunyakirkali@gmail.com>
2024-05-18 23:36:00 +0200

    Configure colorscheme

--- config/catppuccin.nix
+++ config/catppuccin.nix
@@ -2,4 +2,33 @@
   colorschemes.catppuccin = {
     enable = true;
   };
+	colorschemes.catppuccin.settings = {
+    flavour = "mocha";
+    transparent_background = true;
+    integrations = {
+      aerial = true;
+      alpha = true;
+      cmp = true;
+      dashboard = true;
+      flash = true;
+      gitsigns = true;
+      headlines = true;
+      illuminate = true;
+      indent_blankline = { enabled = true; };
+      leap = true;
+      lsp_trouble = true;
+      mason = true;
+      markdown = true;
+      mini = true;
+      neotest = true;
+      neotree = true;
+      noice = true;
+      notify = true;
+      semantic_tokens = true;
+      telescope = true;
+      treesitter = true;
+      treesitter_context = true;
+      which_key = true;
+    };
+  };
 }
