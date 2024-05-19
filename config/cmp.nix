bf939a27327fe1f96ef26711dad7d090fd849beb
Dunya Kirkali <dunyakirkali@gmail.com>
2024-05-19 20:24:40 +0200

    Improve cmp

--- config/cmp.nix
+++ config/cmp.nix
@@ -3,7 +3,9 @@
     enable = true;
     autoEnableSources = true;
   };
+	plugins.lspkind.enable = true;
   plugins.cmp-nvim-lsp.enable = true;
+	plugins.cmp-path.enable = true;
   plugins.cmp-buffer.enable = true;
-  plugins.cmp-path.enable = true;
+	plugins.cmp_luasnip.enable = true;
 }
